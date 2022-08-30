37fa6ab04fbecccbde0bab0bd2a2ba2f880c373f
Ahmed Ahmed <ahmedah@elkjop.no>
2022-08-30 11:50:19 +0200

    create componentes and add styling to files

--- nakamafrontend/src/App.vue
+++ nakamafrontend/src/App.vue
@@ -3,10 +3,31 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav> -->
-  <router-view/>
+  <div>
+    <NavBar></NavBar>
+    <router-view/>
+  </div>
 </template>
 
-<style>
+<script>
+import { defineAsyncComponent } from 'vue'
+const NavBar = defineAsyncComponent(
+  () => import('@/components/NavBar')
+)
+
+export default {
+  name: 'App',
+  props: {
+
+  },
+  components: {
+    NavBar
+  }
+}
+</script>
+
+<!-- <style>
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -27,4 +48,4 @@ nav a {
 nav a.router-link-exact-active {
   color: #42b983;
 }
-</style>
+</style> -->
---
+++ nakamafrontend/src/components/CreatePost.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="h-3/4  w-10/12 bg-nakama-beige flex justify-center rounded-lg shadow-xl">
+        <div class="flex flex-col  w-4/5 mt-10">
+            <p class="text-s text-black mb-4"> Event title:</p>
+            <input type="text" class="border h-10 w-full shadow-sm rounded-lg mb-4" placeholder="Title*">
+             <p class="text-s text-black mb-4"> Event description:</p>
+            <textarea class="border h-16 w-full shadow-sm rounded-lg mb-4"></textarea>
+             <p class="text-s text-black mb-4"> Event type:</p>
+            <input type="text" class="border h-10 w-full shadow-sm rounded-lg mb-4" placeholder="Type*">
+             <p class="text-s text-black mb-4"> Event time / estimated time:</p>
+            <input type="text" class="border h-10 w-full shadow-sm rounded-lg mb-4" placeholder="Time*">
+            <StyledButton :text="'Create post'" class=" flex self-end mt-4"></StyledButton>
+        </div>
+    </div>
+</template>
+
+<script>
+import { defineAsyncComponent } from 'vue'
+const StyledButton = defineAsyncComponent(
+  () => import('@/components/StyledButton')
+)
+
+export default {
+  name: 'PostCard',
+  props: {
+
+  },
+  components: {
+    StyledButton
+}
+}
+</script>
