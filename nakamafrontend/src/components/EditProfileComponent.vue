eda0a1600d717ecc4679710c1cf5a3eefc257156
imanahmedoslo <imanahmedimanahmed@gmail.com>
2022-09-02 12:15:49 +0200

    make popups

---
+++ nakamafrontend/src/components/EditProfileComponent.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="h-fit  w-1/4 bg-white bg-opacity-60 flex justify-center rounded-lg shadow-xl">
+        <div class="flex flex-col  w-4/5 mt-10">
+            <p class="text-s text-black mb-4"> Edit Name:</p>
+            <input type="text" class="border h-10 w-full shadow-sm rounded-lg mb-4" placeholder="Iman Ahmed*">
+             <p class="text-s text-black mb-4"> Edit Age:</p>
+            <input type="text" class="border h-10 w-full shadow-sm rounded-lg mb-4" placeholder="24*">
+             <p class="text-s text-black mb-4"> Edit Company:</p>
+            <input type="text" class="border h-10 w-full shadow-sm rounded-lg mb-4" placeholder="Kiwi Bogstad*">
+             <p class="text-s text-black mb-4"> Edit Job:</p>
+            <input type="text" class="border h-10 w-full shadow-sm rounded-lg mb-4" placeholder="cashier*">
+             <p class="text-s text-black mb-4"> Update Bio:</p>
+            <textarea class="border h-16 w-full shadow-sm rounded-lg mb-4"></textarea>
+             <p class="text-s text-black mb-4"> Edit interests:</p>
+            <input type="text" class="border h-10 w-full shadow-sm rounded-lg mb-4" placeholder="Reading, coding, k-drama and food ^^*">
+            <StyledButton :text="'Update'" class=" flex self-end mt-4 mb-4"></StyledButton>
+        </div>
+    </div>
+</template>
+
+<script>
+import { defineAsyncComponent } from 'vue'
+const StyledButton = defineAsyncComponent(
+  () => import('@/components/StyledButton')
+)
+
+export default {
+//   name: '',
+//   props: {
+
+//   },
+  components: {
+    StyledButton
+}
+}
+</script>
---
+++ nakamafrontend/src/components/FriendsComponent.vue
@@ -0,0 +1,30 @@
+<template>
+   <div class="h-fit w-fit py-2 px-2 rounded-lg bg-white border bg-opacity-60 flex gap-4 flex-col justify-start">
+         <div class="flex flex-row hover:shadow-lg hover:rounded-lg">
+        <img src="../assets/imanProfile.webp" alt="" class="rounded-full h-14 w-14 border-2 border-black ml-2">
+        <button class="text-xl text-black ml-2">Iman Ahmed</button>
+      </div>
+       <div class="flex flex-row hover:shadow-lg hover:rounded-lg">
+        <img src="../assets/ahmedProfilBilde.png" alt="" class="rounded-full h-14 w-14 border-2 border-black ml-2">
+        <button class="text-xl text-black ml-2">Ahmed Ahmed</button>
+      </div>
+      <div class="flex flex-row hover:shadow-lg hover:rounded-lg">
+        <img src="../assets/AmalProfilBilde.png" alt="" class="rounded-full h-14 w-14 border-2 border-black ml-2">
+        <button class="text-xl text-black ml-2">Amal Ahmed</button>
+      </div>
+       <div class="flex flex-row hover:shadow-lg hover:rounded-lg">
+        <img src="../assets/IlhamProfilbilde.png" alt="" class="rounded-full h-14 w-14 border-2 border-black ml-2">
+        <button class="text-xl text-black ml-2">Ilham Ahmed</button>
+      </div>
+        </div>  
+</template>
+
+<!-- <script>
+
+export default {
+  name: 'FriendsComponent',
+  props: {
+
+  },
+}
+</script> -->
