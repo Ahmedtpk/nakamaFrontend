bbd056274f7957967d0c80ca5788de536976ebd0
imanahmedoslo <imanahmedimanahmed@gmail.com>
2022-09-17 13:47:43 +0200

    fix navbar

--- nakamafrontend/src/components/NavBar.vue
+++ nakamafrontend/src/components/NavBar.vue
@@ -1,13 +1,13 @@
 <template>
 <div class="w-screen  flex justify-center ">
   <div class="flex flex-row items-center border-2 h-20 w-11/12 mt-3 bg-[#FEE5E8] mx-2 rounded-md  drop-shadow-xl border-black border-opacity-40">
-    <img class="w-fit ml-4"  src="../assets/image-removebg-preview.png" alt="">
+    <img @click="goToLogedInPage" class="w-fit ml-4"  src="../assets/image-removebg-preview.png" alt="">
     <div class=" flex flex-row ml-auto items-center w-fit h-full">
       <div class="flex flex-row items-center">
         <input type="text" class="w-48 h-10 border rounded-md border-black pl-2" placeholder="Search">
         <img src="../assets/searchIcon.png" alt="" class="w-6 h-6 absolute ml-40">
-         <button class="text-xl text-black ml-4">Iman</button>
-        <img src="../assets/imanProfile.webp" alt="" class="rounded-full h-14 w-14 border-2 border-black ml-4 mr-8">
+         <button @click="goToProfile" class="text-xl text-black ml-4">Iman</button>
+        <img @click="goToProfile" src="../assets/imanProfile.webp" alt="" class="rounded-full h-14 w-14 border-2 border-black ml-4 mr-8 cursor-pointer">
       </div>
       <!-- <div class="flex flex-row items-center">
         <button class="text-xl text-black ml-4">Iman</button>
@@ -21,5 +21,14 @@
 <script>
 export default {
   name: 'NavBar',
+  methods:{
+      goToProfile() {
+      this.$router.push({ name: 'ProfilePage' })
+    },
+    goToLogedInPage(){ 
+      this.$router.push({ name: 'logedInPage' })
+    }
 }
+}
+
 </script>
